9f501b64b3e49a69ac6fc51e478ee8c2c96bc772
thenoveltyseeker <rolbinster@gmail.com>
2024-05-20 02:08:58 +0530

    feat: Basic prompt setup

---
+++ app/src/main/java/dev/thenoveltyseeker/finsights/BakingViewModel.kt
@@ -0,0 +1,86 @@
+package dev.thenoveltyseeker.finsights
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.BlockThreshold
+import com.google.ai.client.generativeai.type.HarmCategory
+import com.google.ai.client.generativeai.type.SafetySetting
+import com.google.ai.client.generativeai.type.content
+import com.google.ai.client.generativeai.type.generationConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class BakingViewModel : ViewModel() {
+    private val _uiState: MutableStateFlow<UiState> =
+        MutableStateFlow(UiState.Initial)
+    val uiState: StateFlow<UiState> =
+        _uiState.asStateFlow()
+
+    private val generativeModel = GenerativeModel(
+        modelName = "gemini-pro-vision",
+        apiKey = BuildConfig.apiKey
+    )
+
+    val model = GenerativeModel(
+        "gemini-1.0-pro",
+        // Retrieve API key as an environmental variable defined in a Build Configuration
+        // see https://github.com/google/secrets-gradle-plugin for further instructions
+        BuildConfig.apiKey,
+        generationConfig = generationConfig {
+            temperature = 1f
+            topK = 64
+            topP = 0.95f
+            maxOutputTokens = 8192
+        },
+        safetySettings = listOf(
+            SafetySetting(HarmCategory.HARASSMENT, BlockThreshold.MEDIUM_AND_ABOVE),
+            SafetySetting(HarmCategory.HATE_SPEECH, BlockThreshold.MEDIUM_AND_ABOVE),
+            SafetySetting(HarmCategory.SEXUALLY_EXPLICIT, BlockThreshold.MEDIUM_AND_ABOVE),
+            SafetySetting(HarmCategory.DANGEROUS_CONTENT, BlockThreshold.MEDIUM_AND_ABOVE),
+        ),
+    )
+
+    fun sendPrompt(
+        bitmap: Bitmap,
+        prompt: String
+    ) {
+        _uiState.value = UiState.Loading
+
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+//                val response = generativeModel.generateContent(
+//                    content {
+//                        image(bitmap)
+//                        text(prompt)
+//                    }
+//                )
+                val response = model.generateContent(
+                    content() {
+                        text("You are a financial advisor providing insights based on the users spending habits, income and age. The suggestions are based on widely popular financial rules like 50/30/20, 100-age, Rule 72 and many more you can think of. Advice be precise and based on the provided income value")
+                        text(prompt)
+                        text("advice: ")
+                    }
+                )
+                response.text?.let { outputContent ->
+                    _uiState.value = UiState.Success(outputContent)
+                }
+            } catch (e: Exception) {
+                _uiState.value = UiState.Error(e.localizedMessage ?: "")
+            }
+        }
+    }
+}
+
+
+
+//// Get the first text part of the first candidate
+//print()
+//// Alternatively
+//print(response.candidates.first().content.parts.first().asTextOrNull())
+
+
---
+++ app/src/main/java/dev/thenoveltyseeker/finsights/MainActivity.kt
@@ -0,0 +1,27 @@
+package dev.thenoveltyseeker.finsights
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import dev.thenoveltyseeker.finsights.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                }
+            }
+        }
+    }
+}
